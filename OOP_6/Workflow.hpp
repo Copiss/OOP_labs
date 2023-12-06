c776b136c36e8cd0ddc1d91374f53cc8347d9ef2
Copiss <glaiborodaxenia@yandex.ru>
2023-12-06 23:14:32 +0200

    +6

---
+++ OOP_6/Workflow.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "Keyboard.hpp"
+class Workflow
+{
+public:
+    explicit Workflow(Keyboard& keyboard) : keyboard(keyboard) {}
+    // Открываем доступ к методам нажатия клавиши и отмены в контексте рабочего процесса (workflow)
+    void keypress(const std::string& key);
+    void undo();
+    // Выполняем все действия в рабочем процессе (workflow)
+    void perform();
+    // Добавляем действие в рабочий процесс (workflow)
+    void addAction(const Action& action);
+private:
+    Keyboard& keyboard;
+    std::vector<Action> actions; // вектор действий для выполнения в рабочем процессе (workflow)
+};
