38ab6b9055b5f3a9e8267e7ea972754bf16bdf29
Copiss <glaiborodaxenia@yandex.ru>
2023-12-27 14:42:43 +0200

    4 исправленная

--- OOP_4_cange/cmake-build-debug/cmake_install.cmake
+++ OOP_4_cange/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/user/Desktop/forGit/OOP_labs/OOP_1
+# Install script for directory: C:/Users/user/Desktop/Prog-frog/OOP_4_cange
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OOP_1")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OOP_4_cange")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/user/Desktop/forGit/OOP_labs/OOP_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/user/Desktop/Prog-frog/OOP_4_cange/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
